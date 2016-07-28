8444174fe0616369558573e181fe1b7287fb8d78
Extensa <ragnar245@gmail.com>
2016-07-28 15:04:39 +0300

    [Back-end] Implemented comment delete functionality.

--- services/Comments/CommentController.js
+++ services/Comments/CommentController.js
@@ -70,7 +70,55 @@ exports.edit = {
                                 reply({ message: 'Comment edited successfuly!' });
                             });
 
-                            
+                        } else {
+                            reply(Boom.forbidden('You don\'t have permission to edit this comment.'));
+                        }
+                    } else {
+                        reply(Boom.notFound('The comment you are trying to edit does not exist!'));
+                    }
+                });
+            } else {
+                reply(Boom.notFound('The post you are searching for does not exist!'));
+            }
+        });
+    },
+    validate: {
+        payload: Joi.object({
+            content: Joi.string().min(3).required()
+        })
+    },
+    auth: {
+        strategy: 'jwt'
+    }
+}
+
+exports.remove = {
+    handler: (request, reply) => {
+        let postId = request.params.postId,
+            commentId = request.params.commentId;
+        
+        Post.findById(postId, (err, post) => {
+            if (err) { reply(Boom.badRequest(err)); }
+
+            if (post) {
+                Comment.findById(commentId, (err, comment) => {
+                    if (err) { reply(Boom.badRequest(err)); }
+
+                    if (comment) {
+                        var credentials = request.auth.credentials,
+                            canDelete = credentials.id === comment.creator ||
+                                        credentials.scope === 'admin' ||
+                                        credentials.scope === 'manager';
+
+                        if (canDelete) {
+                            comment.remove((err) => {
+                                if (err) { reply(Boom.badRequest(err)); }
+
+                                var commentIndex = post.comments.indexOf(comment._id);
+                                post.comments.splice(commentIndex, 1);
+
+                                reply({ message: 'Comment deleted successfuly!' });
+                            });
                         } else {
                             reply(Boom.forbidden('You don\'t have permission to edit this comment.'));
                         }
--- services/Comments/CommentRoutes.js
+++ services/Comments/CommentRoutes.js
@@ -4,5 +4,6 @@ var CommentController = require('./CommentController');
 
 exports.endpoints = [
     { method: 'POST', path: '/api/posts/{postId}/comments', config: CommentController.create },
-    { method: 'PUT', path: '/api/posts/{postId}/comments/{commentId}', config: CommentController.edit }
+    { method: 'PUT', path: '/api/posts/{postId}/comments/{commentId}', config: CommentController.edit },
+    { method: 'DELETE', path: '/api/posts/{postId}/comments/{commentId}', config: CommentController.remove }
 ]
